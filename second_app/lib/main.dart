f8789953b52ea92a0cf1213f047783e7196d6001
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2023-09-05 17:08:54 +0300

    first commit

---
+++ second_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Koodi rakentuu kahdesta erilaisista "sanoista"
+// 1. keywords, ohjelmointi kieli määrittelee
+// 2. identifiers, ohjelmoijat määrittelevät
+
+void main() {
+  // Määritellään funktio, tämä "main" on dart kielen aloitus piste
+
+  // function body
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'), // Pilkut sulkujen jälkeen!!!
+        ),
+      ),
+    ),
+  ); // Suoritetaan flutter frameworkin funktio
+} // const, data on immutable
+
+void add({num1, num2}) {
+  // Määritellään, named parameters
+  num1 + num2; // 5 ja 3
+}
+
+void test() {
+  add(num1: 5, num2: 3); // Suoritetaan, käytetään named parameters
+}
