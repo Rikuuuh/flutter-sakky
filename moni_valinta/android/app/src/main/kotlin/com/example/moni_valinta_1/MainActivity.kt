fb8af5da939503bd12de0f44a497a7a20062db08
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2023-09-18 20:00:47 +0300

    moni_valinta

---
+++ moni_valinta/android/app/src/main/kotlin/com/example/moni_valinta_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moni_valinta_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ moni_valinta/lib/start_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/question.png',
+            width: 150,
+            color: const Color.fromARGB(115, 255, 253, 137),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            'Start Screen',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt_sharp),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
