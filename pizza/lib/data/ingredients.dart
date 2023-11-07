c9eeacf818cfcb01d47bf548e857155c8d8b264d
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2023-11-07 16:30:21 +0200

    Ingredients-lista up

--- pizza/lib/data/ingredients.dart
+++ pizza/lib/data/ingredients.dart
@@ -1,8 +1,16 @@
 import 'package:pizza/models/ingredient.dart';
 
 const ingredients = [
-  Ingredient('Jauheliha', 62.5, 315.5),
-  Ingredient('Tonnikala', 75.2, 138.8),
-  Ingredient('Herkkusieni', 52.3, 24.5),
-  Ingredient('Katkarapu', 47.4, 95.9),
+  Ingredient('beef\'n\'roast-naudanjauheliha', 62.5, 315.5, 1.00),
+  Ingredient('tonnikala', 75.2, 138.8, 1.00),
+  Ingredient('herkkusieni', 52.3, 24.5, 1.50),
+  Ingredient('katkarapu', 47.4, 95.9, 2.00),
+  Ingredient('feta', 30.1, 69.1, 1.00),
+  Ingredient('ananas', 9, 47.5, 0.50)
+];
+
+const finalingredients = [
+  FinalIngredients('valkosipuli', 0.50),
+  FinalIngredients('kirsikkatomaatti', 1.00),
+  FinalIngredients('Aura@-juusto', 1.00),
 ];
--- pizza/lib/models/ingredient.dart
+++ pizza/lib/models/ingredient.dart
@@ -1,7 +1,16 @@
 class Ingredient {
-  const Ingredient(this.name, this.gramsPerPortion, this.caloriesPer100g);
+  const Ingredient(
+      this.name, this.gramsPerPortion, this.caloriesPer100g, this.price);
 
   final String name;
   final double gramsPerPortion;
   final double caloriesPer100g;
+  final double price;
+}
+
+class FinalIngredients {
+  const FinalIngredients(this.name, this.price);
+
+  final String name;
+  final double price;
 }
