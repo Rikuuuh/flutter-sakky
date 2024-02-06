f586f64994cd55c01b42cff76a85ea45b0655ae8
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2024-02-06 19:32:26 +0200

    Toimiii register&login

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:chat/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -14,7 +20,7 @@ class App extends StatelessWidget {
       title: 'Chat',
       theme: ThemeData().copyWith(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 151, 118, 207),
+          seedColor: const Color.fromARGB(255, 224, 206, 255),
         ),
       ),
       home: const AuthScreen(),
--- chat/lib/screens/auth.dart
+++ chat/lib/screens/auth.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -8,8 +9,31 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  // Onko kirjautuminen vai rekisteröinti
+  var _isLogin = true;
+
+  final _formKey = GlobalKey<FormState>();
+
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+    if (!_isLogin) {
+      FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _enteredEmail, password: _enteredPassword);
+    } else {
+      FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _enteredEmail, password: _enteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,25 +55,54 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Padding(
                   padding: const EdgeInsets.all(16),
                   child: Form(
+                    key: _formKey,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         TextFormField(
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Please enter a valid email address!'; // virhe
+                            }
+                            return null; // ei virhettä
+                          },
                           decoration:
                               const InputDecoration(labelText: 'Email Address'),
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
+                          onSaved: (newValue) {
+                            _enteredEmail = newValue!;
+                          },
                         ),
                         TextFormField(
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be atleast 6 characters long!'; // virhe
+                            }
+                            return null; // ei virhettä
+                          },
                           decoration:
                               const InputDecoration(labelText: 'Password'),
                           obscureText: true,
+                          onSaved: (newValue) {
+                            _enteredPassword = newValue!;
+                          },
                         ),
                         const SizedBox(height: 14),
                         ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Kirjaudu sisään'),
+                          onPressed: _submit,
+                          child: Text(_isLogin ? 'Login' : 'Sign up'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create an account'
+                              : 'I already have an account'),
                         )
                       ],
                     ),
