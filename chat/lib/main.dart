d7d74eadd30451703ddca121f23181aa2a1f8df9
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2024-02-12 19:41:02 +0200

    ++

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:chat/screens/auth.dart';
+import 'package:chat/screens/chat.dart';
+import 'package:chat/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:chat/firebase_options.dart';
@@ -22,8 +25,24 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 224, 206, 255),
         ),
+        appBarTheme: const AppBarTheme(
+          color: Color.fromARGB(255, 224, 206, 255),
+        ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          // Lataa ruutu
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            // Snapshot muuttujassa on dataa, jos käyttäjä on kirjautunut
+            return const ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
--- chat/lib/screens/auth.dart
+++ chat/lib/screens/auth.dart
@@ -1,3 +1,4 @@
+import 'package:chat/screens/chat.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _firebase = FirebaseAuth.instance;
   // Onko kirjautuminen vai rekisteröinti
   var _isLogin = true;
 
@@ -17,7 +19,7 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _formKey.currentState!.validate();
 
     if (!isValid) {
@@ -25,12 +27,24 @@ class _AuthScreenState extends State<AuthScreen> {
     }
 
     _formKey.currentState!.save();
-    if (!_isLogin) {
-      FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _enteredEmail, password: _enteredPassword);
-    } else {
-      FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: _enteredEmail, password: _enteredPassword);
+    try {
+      if (!_isLogin) {
+        // Rekisteröinti
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      } else {
+        // Kirjautuminen
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+      }
+    } on FirebaseAuthException catch (error) {
+      // Otetaan vastaan virheilmoitus
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? 'Authentication failed!'),
+        ),
+      );
     }
   }
 
